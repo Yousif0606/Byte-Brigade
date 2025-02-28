53c617775a237e1f931699c95c5a22cdbe716775
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-02-28 16:16:41 -0700

    added tailwind

--- CraftHive/src/App.jsx
+++ CraftHive/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 
+
 const App = () => {
   return (
     <div className="content">
@@ -11,15 +12,15 @@ const App = () => {
 
 const Navbar = () => (
   <nav style={{backgroundColor: theme.colors.primary}} className="w-full p-4 text-white flex justify-between items-center">
-      <div className="flex items-center gap-4">
-          <div className="cursor-pointer">☰</div>
-          <div className="text-xl font-bold">CraftHive</div>
-      </div>
-      <div className="flex items-center gap-4">
-          <div className="cursor-pointer">🔍</div>
-          <div className="cursor-pointer">❓</div>
-      </div>
+    <div className="flex items-center gap-4">
+        <div className="cursor-pointer">☰</div>
+        <div className="text-xl font-bold">CraftHive</div>
+    </div>
+    <div className="flex items-center gap-4">
+        <div className="cursor-pointer">🔍</div>
+        <div className="cursor-pointer">❓</div>
+    </div>
   </nav>
-);
+)
 
 export default App;
