5df3c75a377a4c9cfe18f5a07877313b5f1bef4a
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-03-11 16:33:56 -0600

    updated app to see is things show up correctly.

--- CraftHive/src/App.jsx
+++ CraftHive/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
+import Navbar from './Navbar';
 
 
-const App = () => {
+/*const App = () => {
   return (
     <div className="content">
       <h1>This is the start</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
-};
+};*/
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
 
 export default App;
