f5e2fadfc7b42468b7383a7e56b168dfdf2567c0
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-03-11 18:06:51 -0600

    added comment to index about Brett's suggestion

--- CraftHive/src/index.jsx
+++ CraftHive/src/index.jsx
@@ -8,3 +8,23 @@ root.render(
     <App />
   </React.StrictMode>,
 );
+
+
+// Brett changed the code to this
+/* 
+import React from 'react';
+import Router from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <router>
+      <Aboutpage path="/about" />
+      <Homepage path="/" />
+    </router>
+    <App />
+  </React.StrictMode>,
+);
+*/
