19cec053e095cba52f58703cb437da4118e6bc65
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-03-14 15:24:28 -0600

    swapped the commented out code back to original

--- CraftHive/src/index.jsx
+++ CraftHive/src/index.jsx
@@ -1,7 +1,7 @@
 
 //original code
 
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -10,12 +10,12 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);*/
+);
 
 
 // Brett changed the code to this
  
-import React from 'react';
+/*import React from 'react';
 import Router from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -29,4 +29,4 @@ root.render(
     </router>
     <App />
   </React.StrictMode>,
-);
+);*/
