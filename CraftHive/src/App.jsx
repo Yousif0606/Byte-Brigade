38d400496ac38aefb3197dc28ff14768d0151b17
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-03-14 15:24:16 -0600

    swapped the commented out code back to original

--- CraftHive/src/App.jsx
+++ CraftHive/src/App.jsx
@@ -2,25 +2,25 @@ import './App.css';
 import Navbar from './Navbar';
 
 // This is original App component ~ Ryleigh
-/*const App = () => {
+const App = () => {
   return (
     <div className="content">
       <h1>This is the start</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
-};*/
+};
 
 
 // This is the modified App component based on 
 // https://medium.com/@mechiranthan.y/a-step-by-step-guide-to-building-your-first-web-app-with-react-js-ad7bd6fe6e1b
 //  ~ Ryleigh 
-function App() {
+/*const App = () => {
   return (
     <div className="App">
       <Navbar />
     </div>
   );
-}
+}*/
 
 export default App;
