122321c08e031b583b675ddbd187c50c83c24c7d
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-04-03 19:00:49 -0600

    code generated from figma to app

--- CraftHive/src/App.jsx
+++ CraftHive/src/App.jsx
@@ -7,12 +7,33 @@ const App = () => {
     <div className="content">
       <h1>Welcome to CraftHive!</h1>
       <p>Created by ByteBrigade with rsbuild.</p>
-      <div class="inline-block group p-6 border-2 cursor-pointer hover:bg-gray-50 hover:border-blue-100">
-        <button type='button' className='class="shadow h-16 w-13 m-4 bg-teal-400 inline-flex"'>Click this button here</button> 
+      {/* <div className="inline-block group p-6 border-2 cursor-pointer hover:bg-gray-50 hover:border-blue-100"> */}
+        {/* <button type='button' className='shadow h-16 w-13 m-4 bg-teal-400 inline-flex'>Click this button here</button>  */}
+
+<div style={{width: '100%', height: '100%', position: 'relative', background: '#F7F2FF', overflow: 'hidden'}}>
+          <div style={{width: 1259, height: 74, left: 99, top: 213, position: 'absolute'}}>
+            <span style={{color: 'black', fontSize: 55, fontFamily: 'Lexend', fontWeight: '300', wordWrap: 'break-word'}}>Welcome to </span><span style={{color: '#5B3E8C', fontSize: 55, fontFamily: 'Lexend', fontWeight: '600', wordWrap: 'break-word'}}>CraftHive!</span></div>
+          <div style={{width: 1368, height: 147, left: 99, top: 275, position: 'absolute', color: 'black', fontSize: 104, fontFamily: 'Lexend', fontWeight: '700', wordWrap: 'break-word'}}>What can I do for you?</div>
+          <div style={{width: 1440, height: 133, left: 0, top: 0, position: 'absolute', background: '#5B3E8C', borderBottomRightRadius: 40, borderBottomLeftRadius: 40}} />
+          <div style={{width: 65, height: 65, left: 1325, top: 34, position: 'absolute', overflow: 'hidden'}}>
+          <div style={{width: 65, height: 65, left: 0, top: 0, position: 'absolute', background: 'white'}} />
+        </div>
+        <div style={{width: 65, height: 65, left: 1218, top: 34, position: 'absolute', borderRadius: 9999, border: '7px white solid'}} />
+          <div style={{width: 45, height: 50, left: 1228, top: 47, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 55, fontFamily: 'Myanmar Khyay', fontWeight: '400', wordWrap: 'break-word'}}>?</div>
+          <div style={{width: 380, height: 380, left: 105, top: 524, position: 'absolute'}}>
+          <div style={{width: 380, height: 380, left: 0, top: 0, position: 'absolute', background: '#FFB845', borderRadius: 30}} />
+          <div style={{width: 380, height: 74, left: 0, top: 250, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 30, fontFamily: 'Lexend', fontWeight: '600', wordWrap: 'break-word'}}>Suggest <br/>product pricing</div>
+          <div style={{width: 169, height: 169, left: 105, top: 41, position: 'absolute', background: 'white'}} />
+          <div style={{width: 28.98, height: 28.98, left: 126.01, top: 62.02, position: 'absolute', background: 'white'}} />
+          <div style={{width: 65.54, height: 69.24, left: 164.54, top: 99.79, position: 'absolute', background: 'white'}} />
+        </div>
       </div>
+
+
+      {/* </div> */}
       <br></br>
       <div>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        
       </div>
     </div>
   );
