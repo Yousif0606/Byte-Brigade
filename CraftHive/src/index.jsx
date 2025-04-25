03e46604868b02124a1aff4ade67e30add246640
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-04-25 12:50:11 -0600

    installed react bootstrap and addjusted navbar

--- CraftHive/src/index.jsx
+++ CraftHive/src/index.jsx
@@ -4,13 +4,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Navbar from './Navbar';
 import FeatureCard from './HomePage/FeatureCard';
 import FeatureGrid from './HomePage/FeatureGrid';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < App/>
+    < Navbar/>
   </React.StrictMode>,
 );
 
