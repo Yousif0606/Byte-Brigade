04e89d131c80d922b033e96a3eae322c1d66d11b
RDeue204 <144149483+MyLoverisTheMoon@users.noreply.github.com>
2025-04-27 19:05:27 -0600

    updated package.json to match nate's branch

--- CraftHive/src/index.jsx
+++ CraftHive/src/index.jsx
@@ -11,7 +11,7 @@ import FeatureGrid from './HomePage/FeatureGrid';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < Navbar/>
+    < App/>
   </React.StrictMode>,
 );
 
