3934e597709e19a1c5f9f033457c6670624b0282
Nate Shearer <127697024+shearernas@users.noreply.github.com>
2025-05-02 15:42:46 -0600

    Landing page for price suggestion complete

--- CraftHive/src/screens/PricingMaterialInput.jsx
+++ CraftHive/src/screens/PricingMaterialInput.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import RequirementsList from "../components/RequirementsList";
+import MaterialsDescription from "../components/MaterialsDescription";
 import NavigationButtons from "../components/NavigationButtons";
 
 /* Below is an example of calling a function from a JS helper file, and passing an argument to it from this file -Nate
@@ -8,16 +8,23 @@ const renatoSayHi = () => alert('hi renato');*/
 
 const PricingMaterialInput = () => {
   return (
-      <section className="flex flex-col items-center">
-        <h1 className="header-text mt-12 text-6xl font-medium text-purple-900 max-md:text-5xl max-sm:text-4xl">
-          Suggest product pricing
-        </h1>
-        <h2 className="header-text mx-0 my-5 text-9xl font-bold text-black max-md:text-8xl max-sm:text-7xl">
-          Let's get started!!!!!!!
-        </h2>
-        <RequirementsList />
-        <NavigationButtons showBackButton={false} nextRoute="/pricing/hoursinput" />
-      </section>
+    <section className="flex flex-col w-full">
+    {/*<Logo className="background-icon" />*/}
+    <div className="ml-8 md:ml-32">
+      <h1 className="header-text py-14 text-5xl font-semibold purple-text-main max-md:text-5xl max-sm:text-4xl">
+        Suggest product pricing
+      </h1>
+    </div>
+
+    <div className="flex-grow flex flex-col">
+      <MaterialsDescription />
+      <div className="mt-auto w-full">
+        <NavigationButtons nextRoute="/pricing/materialinput" />
+      {/*Put in tag above for toggleable visibility}*/}
+      {/*showBackButton={false}*/}
+      </div>
+    </div>
+  </section>
   );
 }
 
